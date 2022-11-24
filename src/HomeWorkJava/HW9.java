eeb0487e46176e98c59d659f8bfc9940a691f280
stoczec <stoczec@gmail.com>
2022-11-24 16:37:27 +0100

    Send files

---
+++ src/HomeWorkJava/HW9.java
@@ -0,0 +1,35 @@
+package HomeWorkJava;
+
+public class HW9 {
+    public static void main(String[] args) {
+        // Level 1:
+        // Дано целое число, не меньшее 2.
+        // Выведите его НАИБОЛЬШИЙ делитель, (число, на которое введенное число делится без остатка),
+        // отличный от самого этого числа.
+
+        int number = 15; // !<2
+        for (int i = number - 1; i > 1; i--) {
+            if (number % i == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+        // Level 2:
+        // Вклад в банке составляет x рублей. Ежегодно он увеличивается на p процентов, после чего дробная часть копеек отбрасывается.
+        //Каждый год сумма вклада становится больше. Определите, сколько будет на счету через k лет.
+        //До начала программирования решите эту задачу на бумаге - возьмите p=10, k=2, x = 1000
+
+        int deposit = 189987;
+        int percent = 10;
+        int years = 15;
+        double kfPercent = ((double) percent / 100) + 1;
+        double account = 0;
+
+        for (int i = 1; i <= years; i++) {
+            if (i == 1) {
+                account = deposit * kfPercent;
+            } else account = (int) account * kfPercent;
+        }
+        System.out.println((int) account);
+    }
+}
---
+++ src/Practice/Lesson09_23_11_22.java
@@ -0,0 +1,62 @@
+package Practice;
+
+public class Lesson09_23_11_22 {
+    public static void main(String[] args) {
+       /* Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }
+        do {
+            System.out.println(x);
+            x--;
+
+        }
+        while (x > 0);
+        // цикл while может выполнится ни разу
+        // но цикл do-while минимум одни раз
+        // отличаютмся только на 1 итерации
+        //Без остановки вводится в программу число
+        //Остановить считование новых чисел, когда будет введено отрицательное число
+        Scanner scannerNew = new Scanner(System.in);
+        int number;
+        do
+
+        {
+            number = scannerNew.nextInt();
+        }while(number>0);
+
+        int i = 0;
+        while (i < 100){
+            System.out.println(i);
+            i++;
+            for (int i = 0; i < 100; i++) {
+                System.out.println(i);
+            }
+
+
+    }
+}
+
+
+    }
+
+
+
+
+                }*/
+        //вывести числа от 1000 до 10000, которые кончаются на 4
+        //усложнение: вывести числа от 1000 до 10000, которые кончаются на 4 и на 9
+
+        /*for (int i = 1004; i < 10000; i+=10){
+            System.out.println(i);
+
+        }*/
+        for (int i = 1004; i < 10000; i += 5) {
+            System.out.println(i);
+        }
+    }
+
+}
+
