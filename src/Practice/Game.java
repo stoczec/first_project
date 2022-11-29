95d9aad53a89350a437206065345989fe2bc4635
stoczec <stoczec@gmail.com>
2022-11-29 18:08:33 +0100

    Send files

--- src/Practice/Game.java
+++ src/Practice/Game.java
@@ -10,13 +10,7 @@ public class Game {
                 {"Почти!", "Уже близко!", "Вы нашли клад!", "Уже близко!", "Почти!"},
                 {"Далековато!", "Почти!", "Уже близко!", "Почти!", "Далековато!"}
         };
-        System.out.println("""
-                У вас есть возможность найти клад. \s
-                Введите цифрой, направление движения:\s
-                1) Вверх\s
-                2) Вниз\s
-                3) Налево\s
-                4) Направо""");
+        startTitle();
         int len = array.length;
         for (int i = 0; i < len; ) {
             for (int j = 0; j < array[i].length; ) {
@@ -28,40 +22,28 @@ public class Game {
                     i--;
                     if (i < 0) {
                         i++;
-                        System.out.println("""
-                                 Вы вышли за границы игровой зоны.
-                                 Вы начинаете с предыдущей позиции.
-                                """);
+                        warning();
                     }
                     System.out.println(array[i][j]);
                 } else if (choice == 2) {
                     i++;
                     if (i >= len) {
                         i--;
-                        System.out.println("""
-                                 Вы вышли за границы игровой зоны.
-                                 Вы начинаете с предыдущей позиции.
-                                """);
+                        warning();
                     }
                     System.out.println(array[i][j]);
                 } else if (choice == 3) {
                     j--;
                     if (j < 0) {
                         j++;
-                        System.out.println("""
-                                 Вы вышли за границы игровой зоны.
-                                 Вы начинаете с предыдущей позиции.
-                                """);
+                        warning();
                     }
                     System.out.println(array[i][j]);
                 } else if (choice == 4) {
                     j++;
                     if (j >= array[i].length) {
                         j--;
-                        System.out.println("""
-                                 Вы вышли за границы игровой зоны.
-                                 Вы начинаете с предыдущей позиции.
-                                """);
+                        warning();
                     }
                     System.out.println(array[i][j]);
                 }
@@ -72,6 +54,22 @@ public class Game {
             }
         }
 
+    }
+    public static void startTitle(){
+        System.out.println("""
+                У вас есть возможность найти клад. \s
+                Введите цифрой, направление движения:\s
+                1) Вверх\s
+                2) Вниз\s
+                3) Налево\s
+                4) Направо""");
+    }
+
+    public static void warning(){
+        System.out.println("""
+                                 Вы вышли за границы игровой зоны.
+                                 Вы начинаете с предыдущей позиции.
+                                """);
     }
 
 }
